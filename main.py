7d92ee18e649c82052eb5d27443fbb809e2fdf56
Kosmostars7403 <64686091+Kosmostars7403@users.noreply.github.com>
2021-02-09 01:06:26 +0300

    Fix functions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import subprocess
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Сбор активной аудитории по социальным сетям VK, Facebook, Instagram')
-    parser.add_argument('social_network', help='Выберите социальную сеть, варианты: instagram, vk, facebook')
+    parser.add_argument('social_network', choices=['instagram', 'vk', 'facebook'], help='Выберите социальную сеть.')
     social_network = parser.parse_args().social_network
 
     subprocess.call(['python3', f'{social_network}.py'])
--- vk.py
+++ vk.py
@@ -27,24 +27,27 @@ def get_group_id(groupname):
     return -response['response'][0]['id']
 
 
-def paginate_vk_entities(url, payload, pages_amount):
-    paginate_items = []
+def fetch_paginated_entities(url, payload, pages_amount):
+    response = handle_request(url, payload)
+    entities = response['response']['items']
+
+    if not pages_amount:
+        pages_amount = calculate_pages_amount(response['response']['count'])
 
     for page_number in range(1, pages_amount):
-
         payload['offset'] = 100 * page_number
         response = handle_request(url, payload)
 
-        paginate_items += response['response']['items']
+        entities += response['response']['items']
 
-    return paginate_items
+    return entities
 
 
-def get_vk_posts(pages_amount=0):
+def get_vk_posts(groupname, pages_amount=None):
     url = 'https://api.vk.com/method/wall.get'
 
     payload = {
-        'domain': 'cocacola',
+        'domain': groupname,
         'filter': 'owner',
         'offset': 0,
         'count': 100,
@@ -52,18 +55,12 @@ def get_vk_posts(pages_amount=0):
         'v': '5.126'
     }
 
-    response = handle_request(url, payload)
-    posts = response['response']['items']
-
-    if pages_amount == 0:
-        pages_amount = calculate_pages_amount(response['response']['count'])
-
-    posts += paginate_vk_entities(url, payload, pages_amount)
+    posts = fetch_paginated_entities(url, payload, pages_amount)
 
     return posts
 
 
-def get_post_comments(post_id, pages_amount=0):
+def get_post_comments(post_id, pages_amount=None):
     url = 'https://api.vk.com/method/wall.getComments'
 
     payload = {
@@ -76,13 +73,7 @@ def get_post_comments(post_id, pages_amount=0):
         'v': '5.126'
     }
 
-    response = handle_request(url, payload)
-    comments = response['response']['items']
-
-    if pages_amount == 0:
-        pages_amount = calculate_pages_amount(response['response']['count'])
-
-    comments += paginate_vk_entities(url, payload, pages_amount)
+    comments = fetch_paginated_entities(url, payload, pages_amount)
 
     return comments
 
@@ -98,16 +89,6 @@ def filter_comments_by_period(comments, days):
             filtered_comments.append(comment)
 
     return filtered_comments
-
-
-def get_commentators(comments):
-    commentators = set()
-
-    for comment in comments:
-        if 'from_id' in comment:
-            commentators.add(comment['from_id'])
-
-    return commentators
 
 
 def get_likers(post_id):
@@ -136,14 +117,15 @@ if __name__ == '__main__':
     target_group_id = get_group_id(group_name)
     today = date.today()
 
-    posts = get_vk_posts()
+    posts = get_vk_posts(group_name,1)
 
     recent_commentators = set()
     likers = set()
 
     for post in posts:
         filtered_comments = filter_comments_by_period(get_post_comments(post['id']), FILTER_PERIOD_IN_DAYS)
-        recent_commentators = recent_commentators | get_commentators(filtered_comments)
+        commentators = {comment['from_id'] for comment in filtered_comments if 'from_id' in comment}
+        recent_commentators = recent_commentators | commentators
         likers = likers | set(get_likers(post['id']))
 
     core_audience = recent_commentators.intersection(likers)
